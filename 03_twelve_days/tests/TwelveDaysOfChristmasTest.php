29a9a946578fbf0125ae23d2627d4fd531c81f0b
Benjamin Weston <benjamin.weston@sykescottages.co.uk>
2024-11-22 14:55:44 +0000

    Write tests

--- 03_twelve_days/tests/TwelveDaysOfChristmasTest.php
+++ 03_twelve_days/tests/TwelveDaysOfChristmasTest.php
@@ -1,15 +1,40 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Tests;
 
-use PHPUnit\Framework\Attributes\CoversNothing;
+use App\TwelveDaysOfChristmas;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
-#[CoversNothing]
+#[CoversClass(TwelveDaysOfChristmas::class)]
 class TwelveDaysOfChristmasTest extends TestCase
 {
-    public function test_the_truth(): void
+    public function testInvalidArguments() :void {
+        $tdc = new TwelveDaysOfChristmas();
+        $this->expectException(\InvalidArgumentException::class);
+        $tdc->twelveDaysOfChristmas(0);
+        $tdc->twelveDaysOfChristmas(13);
+    }
+
+    #[DataProvider('inputOutputMappingProvider')]
+    public function testExhaustively(int $input, string $output): void
     {
-        $this->assertTrue(true);
+        $tdc = new TwelveDaysOfChristmas();
+        $this->assertEquals($output, $tdc->twelveDaysOfChristmas($input));
+    }
+
+    /**
+     * @return array<array<int,string>>
+     */
+    public static function inputOutputMappingProvider(): array
+    {
+        return [
+            [1, "On the first day of Christmas,\nMy true love gave to me:\nA partridge in a pear tree."],
+            [2, "On the second day of Christmas,\nMy true love gave to me:\nTwo turtle doves,\nAnd a partridge in a pear tree."],
+            [12, "On the twelfth day of Christmas,\nMy true love gave to me:\nTwelve drummers drumming,\nEleven pipers piping,\nTen lords a-leaping,\nNine ladies dancing,\nEight maids a-milking,\nSeven swans a-swimming,\nSix geese a-laying,\nFive golden rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree."],
+        ];
     }
 }
